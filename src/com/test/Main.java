764bfbf5cb3e75a4a7cc40411a423d1374bd40b3
Alex <alexchuon81500@gmail.com>
2017-10-03 10:29:33 -0400

    boom

--- src/com/test/Main.java
+++ src/com/test/Main.java
@@ -1,30 +1,43 @@
 package com.test;
 
 import javafx.application.Application;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.scene.Scene;
-import javafx.stage.Stage
+import javafx.stage.Stage;
+import javafx.scene.layout.HBox;
+import javafx.scene.control.ToggleButton;
 
 public class Main extends Application {
 
-    public static void Main (String[] args) { System.out.println("You got Rick Rolled my guy..");
+    public static void RickRoll_click() { System.out.println("You got Rick Rolled my guy..");}
 
-    @Override
+    //@Override
     public void start(Stage primaryStage) throws Exception {
-        Button RickRoll = new Button ("Rick Roll?");
+        Button RickRoll = new Button("Click Here");
 
-        RickRoll.setOnAction(e->RickRoll_click());
+        Image image = new Image("RickRolly.png");
+        ImageView imageView = new ImageView(image);
+
+        HBox hbox = new HBox(imageView);
+
+        RickRoll.setOnAction(e -> RickRoll_click());
 
         StackPane myUIFrame = new StackPane();
+        myUIFrame.getChildren().add(hbox);
         myUIFrame.getChildren().add(RickRoll);
 
-        Scene scene = new Scene(myUIFrame, 400,400);
+
+        Scene scene = new Scene(myUIFrame, 1000, 900);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("My First GUI");
+        primaryStage.show();
+    }
 
 
-
-
+    public static void main(String[] args) { Application.launch(args);}
 
         }
-}
+
