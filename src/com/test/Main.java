95ec9316587a52ee5fbd9366e63fc4c330c85800
Alex <alexchuon81500@gmail.com>
2017-10-10 10:16:50 -0400

    boom

--- src/com/test/Main.java
+++ src/com/test/Main.java
@@ -9,33 +9,94 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.layout.HBox;
 import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
+
 
 public class Main extends Application {
 
-    public static void RickRoll_click() { System.out.println("You got Rick Rolled my guy..");}
+    static StackPane myUIFrame;
+    static HBox rickrollhbox;
+    static Stage myPrimaryStage;
+    static Button RickRoll;
+    static HBox TriggerTrigger;
+    static Image imageLeft;
+    static Image imageRight;
+    static Image imageDown;
+    static Image imageUp;
+    static Image imageMain;
+    static ImageView imageVew;
 
+    public static void RickRoll_click() {
+        System.out.println("You got Rick Rolled my guy..");
+        imageVew.setImage(imageMain);
+    }
+
+    public static void nextpic_click() {
+        System.out.println("You got Rick Rolled my guy..");
+
+        imageVew.setImage(imageLeft);
+
+    }
+
+    public static void nextpic2_click() {
+        System.out.println("You got Rick Rolled my guy..");
+        imageVew.setImage(imageRight);
+
+  }
+    public static void nextpic3_click() {
+        System.out.println("You got Rick Rolled my guy..");
+        imageVew.setImage(imageUp);
+    }
+    public static void nextpic4_click() {
+        System.out.println("You got Rick Rolled my guy..");
+        imageVew.setImage(imageDown);
+    }
     //@Override
     public void start(Stage primaryStage) throws Exception {
-        Button RickRoll = new Button("Click Here");
+        myPrimaryStage = primaryStage;
 
-        Image image = new Image("RickRolly.png");
-        ImageView imageView = new ImageView(image);
+        RickRoll = new Button("Click Here");
 
-        HBox hbox = new HBox(imageView);
+        imageMain = new Image("RickRolly.png");
+        imageLeft = new Image("RickLeft.jpg");
+        imageRight = new Image("RickRight.png");
+        imageDown = new Image("RickRollDown.png");
+        imageUp = new Image("RickRollUp.jpg");
+        imageVew = new ImageView();
+        ToggleButton toggleButton1 = new ToggleButton("Left");
+        ToggleButton toggleButton2 = new ToggleButton("Right");
+        ToggleButton toggleButton3 = new ToggleButton("Up");
+        ToggleButton toggleButton4 = new ToggleButton("Down");
+
+        ToggleGroup toggleGroup = new ToggleGroup();
+
+        toggleButton1.setToggleGroup(toggleGroup);
+        toggleButton2.setToggleGroup(toggleGroup);
+        toggleButton3.setToggleGroup(toggleGroup);
+        toggleButton4.setToggleGroup(toggleGroup);
+
+        TriggerTrigger = new HBox(toggleButton1, toggleButton2, toggleButton3, toggleButton4);
+        rickrollhbox = new HBox(imageVew);
+        //imageDown = new HBox(imageVew);
+
+
 
         RickRoll.setOnAction(e -> RickRoll_click());
+        toggleButton1.setOnAction(e->nextpic_click());
+        toggleButton2.setOnAction(e->nextpic2_click());
+        toggleButton3.setOnAction(e->nextpic3_click());
+        toggleButton4.setOnAction(e->nextpic4_click());
 
-        StackPane myUIFrame = new StackPane();
-        myUIFrame.getChildren().add(hbox);
+        myUIFrame = new StackPane();
+        myUIFrame.getChildren().add(rickrollhbox);
+        myUIFrame.getChildren().add(TriggerTrigger);
         myUIFrame.getChildren().add(RickRoll);
-
 
         Scene scene = new Scene(myUIFrame, 1000, 900);
         primaryStage.setScene(scene);
         primaryStage.setTitle("My First GUI");
         primaryStage.show();
     }
-
 
     public static void main(String[] args) { Application.launch(args);}
 
